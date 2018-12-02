a33ec411ad44bb2880a710d59e355a0763882198
Alber Attard <albert@javacreed.com>
2018-12-02 11:35:43 +0100

    Clean up the project

--- src/main/java/com/javacreed/examples/gson/part4/Main.java
+++ src/main/java/com/javacreed/examples/gson/part4/Main.java
@@ -1,22 +1,3 @@
-/*
- * #%L
- * Gson TypeAdapter Example Serialise Large Objects
- * %%
- * Copyright (C) 2012 - 2015 Java Creed
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package com.javacreed.examples.gson.part4;
 
 import java.io.BufferedWriter;
@@ -30,7 +11,7 @@ import com.google.gson.GsonBuilder;
 
 public class Main {
   public static void main(final String[] args) throws IOException {
-    // Configure GSON
+    /* Configure GSON */
     final GsonBuilder gsonBuilder = new GsonBuilder();
     gsonBuilder.registerTypeAdapter(LargeData.class, new LargeDataSerialiser());
     gsonBuilder.setPrettyPrinting();
@@ -44,7 +25,8 @@ public class Main {
     dir.mkdirs();
 
     try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(dir,
-        "output.json")), "UTF-8"))) {
+                                                                                                      "output.json")),
+                                                                        "UTF-8"))) {
       gson.toJson(data, out);
     }
 
